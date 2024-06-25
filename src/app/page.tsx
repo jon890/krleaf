615a15e9ac376ca25eb3f4452fa949edc6c8b8aa
bifosdev <jon890@naver.com>
2024-06-26 00:33:37 +0900

    메인 푸터

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -453,6 +453,48 @@ export default function Home() {
           />
         </div>
       </section>
+
+      <footer className="w-full bg-[#212B4E] py-10">
+        <div className="container">
+          <div className="flex flex-row justify-between items-center">
+            <Image
+              src="/images/logo/logo_white.png"
+              width="338"
+              height="53"
+              alt="logo_white"
+            />
+
+            <div className="flex flex-row text-white text-base gap-[10px]">
+              <div className="bg-krflea_text_primary rounded-3xl px-5 py-3">
+                개인정보처리방침
+              </div>
+              <div className="border rounded-3xl px-5 py-3 border-opacity-30">
+                저작권 보호정책
+              </div>
+              <div className="border rounded-3xl px-5 py-3 border-opacity-30">
+                이메일 무단수집 거부
+              </div>
+              <div className="border rounded-3xl px-5 py-3 border-opacity-30">
+                사이트맵
+              </div>
+            </div>
+          </div>
+
+          <div className="h-[1px] bg-white bg-opacity-30 my-8"></div>
+
+          <div className="flex flex-col">
+            <p className="text-base text-[#A0A8C2]">
+              (30130) 세종특별자치시 한누리대로 193, 참미르빌딩 8층
+              한국지방교육행정연구재단
+            </p>
+            <p className="text-base text-[#A0A8C2]">FAX : 044-550-5299</p>
+            <p className="text-base text-[#A0A8C2]">Email : kilef@cju.ac.kr</p>
+            <p className="mt-4 text-base text-[#677195]">
+              Copyright 2024 KRFLEA All Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
