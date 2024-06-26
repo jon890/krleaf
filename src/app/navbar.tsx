3e051a8bb9d582a07c96d46d2987cf3cb553f97e
bifosdev <jon890@naver.com>
2024-06-26 22:36:54 +0900

    네비게이션 반응형 적용

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,27 +2,31 @@ import { FONT_PRETENDARD } from "@/lib/font-util";
 import { cn } from "@/lib/utils";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
     <nav
       className={cn(
-        "top-0 fixed w-full text-black font-bold h-[100px] flex flex-row items-center justify-center z-10",
+        "top-0 fixed w-full text-black font-bold h-[100px] flex flex-row items-center justify-center z-10 bg-white bg-opacity-50 backdrop-blur",
         FONT_PRETENDARD.className
       )}
     >
-      <div className="flex-none mr-24">
-        <Image src="/images/logo.png" alt="logo" width="259" height="39" />
-      </div>
+      <Link
+        className="flex-none mr-24 md:mr-12 relative w-[18vw] h-[40px] max-w-[250px] min-w-[200px]"
+        href="/"
+      >
+        <Image src="/images/logo.png" alt="logo" fill sizes="18vw" priority />
+      </Link>
 
-      <ul className="flex flex-row gap-16 justify-center text-xl">
+      <ul className="flex-row gap-16 justify-center text-xl md:text-base hidden md:flex">
         <li>주요사업</li>
         <li>KRFLEA 소식</li>
         <li>정보자료</li>
         <li>KRFLEA 소개</li>
       </ul>
 
-      <div className="ml-32 border-[#BBBBBB] border-2 rounded-3xl flex flex-row gap-3 justify-center items-center py-3 px-4">
+      <div className="ml-32 md:ml-16 border-[#BBBBBB] border-2 rounded-3xl flex-row gap-3 justify-center items-center py-3 px-4 hidden xl:flex">
         <MagnifyingGlassIcon className="size-7" />
         <input
           type="text"
@@ -31,7 +35,7 @@ export default function NavBar() {
         />
       </div>
 
-      <div className="ml-6 border-[#BBBBBB] border-2 rounded-2xl p-2 cursor-pointer">
+      <div className="ml-16 xl:ml-6 border-[#BBBBBB] border-2 rounded-2xl p-2 cursor-pointer">
         <Image
           src="/images/icons/icon_sitemap.png"
           width="34"
