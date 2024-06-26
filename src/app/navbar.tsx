0ef839f681d1aca4f5a1b97dd6226aa68d14a41a
bifosdev <jon890@naver.com>
2024-06-27 00:29:54 +0900

    navbar effect 대응

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -13,17 +13,46 @@ export default function NavBar() {
       )}
     >
       <Link
-        className="flex-none mr-auto relative w-[18vw] h-[40px] max-w-[250px] min-w-[200px]"
+        className="flex-none mr-auto relative w-[18vw] h-[40px] max-w-[250px] min-w-[200px]
+        hover:opacity-50 active:opacity-50 transition-opacity"
         href="/"
       >
         <Image src="/images/logo.png" alt="logo" fill sizes="18vw" priority />
       </Link>
 
       <ul className="flex-row gap-16 justify-center text-xl md:text-base hidden md:flex">
-        <li>주요사업</li>
-        <li>KRFLEA 소식</li>
-        <li>정보자료</li>
-        <li>KRFLEA 소개</li>
+        <li>
+          <Link
+            href="/"
+            className="hover:text-gray-500 active:text-gray-500 transition-colors"
+          >
+            주요사업
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className="hover:text-gray-500 active:text-gray-500 transition-colors"
+          >
+            KRFLEA 소식
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className="hover:text-gray-500 active:text-gray-500 transition-colors"
+          >
+            정보자료
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className="hover:text-gray-500 active:text-gray-500 transition-colors"
+          >
+            KRFLEA 소개
+          </Link>
+        </li>
       </ul>
 
       <div className="ml-32 md:ml-16 border-[#BBBBBB] border-2 rounded-3xl flex-row gap-3 justify-center items-center py-3 px-4 hidden xl:flex">
@@ -35,14 +64,18 @@ export default function NavBar() {
         />
       </div>
 
-      <div className="ml-6 border-[#BBBBBB] border-2 rounded-2xl p-2 cursor-pointer">
+      <button
+        className="ml-6 sm:ml-12 border-[#BBBBBB] border-2 rounded-2xl p-2 cursor-pointer
+      group"
+      >
         <Image
           src="/images/icons/icon_sitemap.png"
           width="34"
           height="34"
           alt="icon_sitemap"
+          className="group-hover:opacity-50 group-active:opacity-50 transition-opacity"
         />
-      </div>
+      </button>
     </nav>
   );
 }
