12693c467702088615abcad2338c02be766416a1
bifos <jon89071@gmail.com>
2024-06-30 16:30:20 +0900

    :lipstick: 모바일 푸터 디자인 적용
    git push

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -2,36 +2,36 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-[#212B4E] py-10">
+    <footer className="w-full bg-[#212B4E] py-12 lg:py-10">
       <div className="container px-4">
-        <div className="flex flex-row justify-between items-center">
+        <div className="flex justify-between items-center flex-col gap-12 lg:flex-row">
           <Image
             src="/images/logo/logo_white.png"
             width="338"
             height="53"
             alt="logo_white"
-            className="max-w-[200px] xl:max-w-[338px]"
+            className="lg:max-w-[338px]"
           />
 
-          <div className="flex xl:flex-row text-white text-base gap-[10px] flex-col">
-            <div className="bg-krflea_text_primary rounded-3xl px-5 py-3 text-xs xl:text-base">
+          <div className="grid grid-cols-2 text-white text-base gap-[10px] lg:flex lg:flex-row">
+            <div className="bg-krflea_text_primary rounded-3xl px-5 py-3 text-base text-center">
               개인정보처리방침
             </div>
-            <div className="border rounded-3xl px-5 py-3 border-opacity-30 text-xs xl:text-base">
+            <div className="border rounded-3xl px-5 py-3 border-opacity-30 text-base text-center">
               저작권 보호정책
             </div>
-            <div className="border rounded-3xl px-5 py-3 border-opacity-30 text-xs xl:text-base">
+            <div className="border rounded-3xl px-5 py-3 border-opacity-30 text-base text-center">
               이메일 무단수집 거부
             </div>
-            <div className="border rounded-3xl px-5 py-3 border-opacity-30 text-xs xl:text-base">
+            <div className="border rounded-3xl px-5 py-3 border-opacity-30 text-base text-center">
               사이트맵
             </div>
           </div>
         </div>
 
-        <div className="h-[1px] bg-white bg-opacity-30 my-8"></div>
+        <div className="hidden lg:block h-[1px] bg-white bg-opacity-30 my-8"></div>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col mt-8 lg:mt-0">
           <p className="text-sm xl:text-base text-[#A0A8C2]">
             (30130) 세종특별자치시 한누리대로 193, 참미르빌딩 8층
             한국지방교육행정연구재단
